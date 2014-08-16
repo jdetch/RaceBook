8c277c53b2cdb2642cc8204e6b0b00014bb05498
Jodi Detch <jodidetch@gmail.com>
2014-08-16 16:32:05 -0400

    Race serializer no longer includes user

--- app/serializers/race_serializer.rb
+++ app/serializers/race_serializer.rb
@@ -1,5 +1,5 @@
 class RaceSerializer < ActiveModel::Serializer
-  attributes :id, :name, :city, :state, :date, :type, :time, :place_agegroup, :place_gender, :place_overall, :note, :user
+  attributes :id, :name, :city, :state, :date, :type, :time, :place_agegroup, :place_gender, :place_overall, :note
   has_many :images
 end
 
