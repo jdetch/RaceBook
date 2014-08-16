ab74e5fb1dd13c49783887414ede77dd28d316f4
Jodi Detch <jodidetch@gmail.com>
2014-08-16 16:53:41 -0400

    App has Rails routes for races and images

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   root :to => 'assets#index'
   get 'assets/index'
 
+  namespace :api do
+    resources :races do
+      resources :images
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
