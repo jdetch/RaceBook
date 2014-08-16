1d32da71dfe8ae9e15fcf527d3cc6ef57ff1ecce
Jodi Detch <jodidetch@gmail.com>
2014-08-16 16:44:03 -0400

    App has a races controller

---
+++ app/controllers/races_controller.rb
@@ -0,0 +1,2 @@
+class RacesController < ApplicationController
+end
---
+++ test/controllers/races_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RacesControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
