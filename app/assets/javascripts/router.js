72eb6812292222e30d23d2de417a2e373cafef8b
Jodi Detch <jodidetch@gmail.com>
2014-08-16 15:09:08 -0400

    App has new routes

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,5 +1,13 @@
 // For more information see: http://emberjs.com/guides/routing/
 
 RaceBook.Router.map(function() {
-  // this.resource('posts');
-});
+  this.route('races');
+  this.route('races.images', { path: 'races/images' });
+  this.route('race.new', { path: 'race/new' });
+  this.resource('race', { path: '/race/:race_id' }, function() {
+    this.route('edit');
+    this.route('delete');
+    this.route('images');
+    });
+  }
+);
