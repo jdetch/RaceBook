464ae88165adb7b24978ca85d70edf7a9a73d942
Jodi Detch <jodidetch@gmail.com>
2014-08-17 17:17:43 -0400

    Routes for images and new race are properly prefaced with a backslash

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -2,8 +2,8 @@
 
 RaceBook.Router.map(function() {
   this.route('races');
-  this.route('races.images', { path: 'races/images' });
-  this.route('race.new', { path: 'race/new' });
+  this.route('races.images', { path: '/races/images' });
+  this.route('race.new', { path: '/race/new' });
   this.resource('race', { path: '/race/:race_id' }, function() {
     this.route('edit');
     this.route('delete');
