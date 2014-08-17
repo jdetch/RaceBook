408ca491f39f2a67825f15ad0338fa80da7b08b8
Jodi Detch <jodidetch@gmail.com>
2014-08-17 13:15:04 -0400

    App has a route and template to display all races to user

---
+++ app/assets/javascripts/routes/races.js
@@ -0,0 +1,5 @@
+RaceBook.RacesRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.find('race');
+  }
+});
--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -5,5 +5,5 @@ RaceBook.Store = DS.Store.extend({
 // Override the default adapter with the `DS.ActiveModelAdapter` which
 // is built to work nicely with the ActiveModel::Serializers gem.
 RaceBook.ApplicationAdapter = DS.ActiveModelAdapter.extend({
-
+  namespace: 'api'
 });
