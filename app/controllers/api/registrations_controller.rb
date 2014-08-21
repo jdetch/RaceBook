2ea3364d56ff0664772a7fd99260893af3827468
Jodi Detch <jodidetch@gmail.com>
2014-08-21 17:00:08 -0400

    App has a registratons controller for restricting access

---
+++ app/controllers/api/registrations_controller.rb
@@ -0,0 +1,15 @@
+class Api::RegistrationsController < ApplicationController
+  before_filter :restrict_access
+  respond_to :json
+
+  private
+
+  # If the block passed to authenticate_or_request_with_http_token returns true, the authentication passes
+  # So, in here we check that an ApiKey exists with the token that is passed in
+  def restrict_acess
+    authenticate_or_request_with_http_token do |token, options|
+      ApiKey.exists?(access_token: token)
+    end
+  end
+
+end
