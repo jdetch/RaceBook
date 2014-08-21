d37c3e521173d5c97307ffe32d682432e9f03416
Jodi Detch <jodidetch@gmail.com>
2014-08-21 12:48:22 -0400

    Ember 'image' model utilizes CloudFront for image url

--- app/assets/javascripts/models/image.js
+++ app/assets/javascripts/models/image.js
@@ -1,4 +1,8 @@
 RaceBook.Image = DS.Model.extend({
-  filename: DS.attr(),
-  race: DS.belongsTo('race')
+  race_image_file_name: DS.attr(),
+  race: DS.belongsTo('race'),
+
+  url: function() {
+    return 'http://d1jb7iycz4uj0c.cloudfront.net/images/'+this.get('id')+'/thumb/'+this.get('race_image_file_name');
+  }.property('race_image_file_name')
 });
