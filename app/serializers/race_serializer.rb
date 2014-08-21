4443ad0e413650fb72b7c3e2035ff030c68ca402
Jodi Detch <jodidetch@gmail.com>
2014-08-21 12:56:19 -0400

    App now properly loads image data from has_many relationship with help from EmbeddedRecordsMixin

--- app/serializers/race_serializer.rb
+++ app/serializers/race_serializer.rb
@@ -1,5 +1,5 @@
 class RaceSerializer < ActiveModel::Serializer
   attributes :id, :name, :city, :state, :date, :race_type, :time, :place_agegroup, :place_gender, :place_overall, :note
-  has_many :images
+  has_many :images, embed: :objects
 end
 
