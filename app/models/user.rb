82988b2291676349814b98663bd28b5cb316d053
Jodi Detch <jodidetch@gmail.com>
2014-08-28 15:04:06 -0400

    Removed username validations bc username field was removed from signup page

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,6 @@ class User < ActiveRecord::Base
   validates_presence_of :password, :on => :create
   validates_presence_of :email
   validates_uniqueness_of :email
-  validates_presence_of :username
-  validates_uniqueness_of :username
 
   def encrypt_password
     if password.present?
